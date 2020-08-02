76af0e7b27ff0501a2703d78e2b6a229a166edd4
Huzaifa Malik <huzaifamalik47@gmail.com>
2020-08-02 13:46:57 +0500

    new with js

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const i = 10;
+switch (i) {
+  case 10:
+    console.log("Value is 10");
+    break;
+  case 15:
+    console.log("Value is 15");
+    break;
+  case 20:
+    console.log("Value is 20");
+    break;
+
+  default:
+    console.log(`the value is ${i}`);
+    break;
+}
